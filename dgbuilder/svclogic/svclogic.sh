ddfb3ee147e9d1f6af9a7d1692d50dae711b0815
Marcus G K Williams <marcus.williams@intel.com>
2017-09-28 14:56:12 -0700

    Fix dgbuilder mysql upload
    Since code moved from sdnc to
    ccsdk dgbuilder has not been able
    to upload or activate directed graphs
    to test them in containers for development
    purposes. This patch updates parts of
    dgbuilder that pointed to sdnc.
    
    Issue-Id: SDNC-92
    Change-Id: I8021312df8c97673ed71232783183e92a06b7bd3
    Signed-off-by: Marcus G K Williams <marcus.williams@intel.com>
    
    Former-commit-id: 5d4de7e28285f2fa503459c398ef9a833227fa86

--- dgbuilder/svclogic/svclogic.sh
+++ dgbuilder/svclogic/svclogic.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #echo "PROJECT_HOME:$PROJECT_HOME"
 
-MYSQL_JDBC_DRIVER=${MYSQL_JDBC_DRIVER:-$PROJECT_HOME/svclogic/lib/mysql-connector-java-5.1.31.jar}
+MYSQL_JDBC_DRIVER=${MYSQL_JDBC_DRIVER:-$PROJECT_HOME/svclogic/lib/mariadb-java-client-2.1.1.jar}
 JARDIR="$PROJECT_HOME/svclogic/lib"
 #JARDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
@@ -11,4 +11,4 @@ do
     CLASSPATH=$CLASSPATH:${jar}
 done
 #echo "CLASSPATH is ${CLASSPATH}"
-java  -cp ${CLASSPATH}:${MYSQL_JDBC_DRIVER}:${SLI_COMMON_JAR} org.openecomp.sdnc.sli.SvcLogicParser $*
+java  -cp ${CLASSPATH}:${MYSQL_JDBC_DRIVER}:${SLI_COMMON_JAR} org.onap.ccsdk.sli.core.sli.SvcLogicParser $*
