ab93e4f6de3d0cc8abb707f98fc11b376a70e6fe
Timoney, Dan (dt5972) <dt5972@att.com>
2018-04-26 14:02:08 -0400

    Move add-apt-repository to startup
    add-apt-repository is failing in Jenkins, so move it to ansible server
    startup script instead.
    
    Change-Id: Iae058003678748c9a4bed9b5e279ebcce4c4695a
    Issue-ID: SDNC-288
    Signed-off-by: Timoney, Dan (dt5972) <dt5972@att.com>
    
    Former-commit-id: 352342232f7578dc31597291947457f800e6619e

--- installation/ansible-server/src/main/scripts/startAnsibleServer.sh
+++ installation/ansible-server/src/main/scripts/startAnsibleServer.sh
@@ -5,6 +5,10 @@ then
 	pip install PyMySQL
 	pip install cherrypy
 	pip install requests
+
+    apt-get -y install software-properties-common
+    apt-add-repository -y ppa:ansible/ansible
+    apt-get -y install ansible
 	date > /tmp/.ansible-server-installed 2>&1
 fi
 
