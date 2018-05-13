c33b5b685c5a2eba2a6a64e7f9c95b3919e0cf8a
Timoney, Dan (dt5972) <dt5972@att.com>
2018-05-13 09:50:34 -0400

    Wrong id used to retrieve status
    LCM DGs were using incoming request id to retrieve status from
    ansible server instead of id returned by ansible server in initial
    request.
    
    Also, ansible should be configured to ignore host key checking to
    avoid certificate issues.
    
    Change-Id: I0cf3d053de652985c4611f8c7d2b5dbee22e0791
    Issue-ID: SDNC-313
    Signed-off-by: Timoney, Dan (dt5972) <dt5972@att.com>
    
    Former-commit-id: 1aac135b0d293dc1e40ff11d56f281f28eddca4b

--- installation/ansible-server/src/main/scripts/startAnsibleServer.sh
+++ installation/ansible-server/src/main/scripts/startAnsibleServer.sh
@@ -3,14 +3,17 @@ exec &> >(tee -a "/var/log/ansible-server.log")
 
 if [ ! -f /tmp/.ansible-server-installed ]
 then
-	pip install PyMySQL
-	pip install cherrypy
-	pip install requests
+    pip install PyMySQL
+    pip install cherrypy
+    pip install requests
 
     apt-get -y install software-properties-common
     apt-add-repository -y ppa:ansible/ansible
     apt-get -y install ansible
-	date > /tmp/.ansible-server-installed 2>&1
+
+    cp /etc/ansible/ansible.cfg /etc/ansible/ansible.cfg.orig
+    cat /etc/ansible/ansible.cfg.orig | sed -e 's/#host_key_checking/host_key_checking/' > /etc/ansible/ansible.cfg
+    date > /tmp/.ansible-server-installed 2>&1
 fi
 
 cd /opt/onap/sdnc
