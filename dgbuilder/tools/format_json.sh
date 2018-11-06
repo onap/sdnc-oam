b9b3b4b74e70cae099a6ccb0dd06427fdb5a2279
Timoney, Dan (dt5972) <dtimoney@att.com>
2018-11-06 13:39:01 -0500

    Remove obsolete dgbuilder
    SDNC dgbuilder is no longer used - dgbuilder is now in CCSDK.
    
    Change-Id: I496e346ed3864779f36c00c354742f7d36447399
    Issue-ID: SDNC-488
    Signed-off-by: Timoney, Dan (dt5972) <dtimoney@att.com>
    
    Former-commit-id: e38fe292bd511784d1c145e84a9af7d94b6f3669

--- dgbuilder/tools/format_json.sh
+++
@@ -1,6 +0,0 @@
-if [ "$#" != "1" ]
-then
-        echo "Usage: $0 jsonFileFullPath"
-        exit
-fi
-cat $1|python -m json.tool
--- dgbuilder/tools/getModuleName.sh
+++
@@ -1,2 +0,0 @@
-module=$(cat $1|egrep "module .*{"|awk '{print $2}'|sed -e 's/{//g')
-echo $module
