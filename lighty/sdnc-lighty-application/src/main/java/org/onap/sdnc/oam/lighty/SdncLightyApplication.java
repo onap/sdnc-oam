817bb02d03296458fc3193b8e375f073b831f98a
Samuel Kontris <samuel.kontris@pantheon.tech>
2019-07-10 18:21:47 +0200

    Proposal to remove OSGi dependencies from the SDNC project
    Dependencies on the OSGi frameworks and libraries are removed
    by integrating the CCSDK and SDNC project with the lighty.io.
    It's a toolkit that allows to use ODL services (in this case
    core services and the Restconf) without the dependency
    on the Karaf framework and the Blueprint DI.
    
    In this change are created the lighty.io modules which
    initialize and expose same services as the Blueprint DI in
    the blueprint.xml files.
    
    More info about the lighty.io - https://lighty.io
    
    Signed-off-by: Samuel Kontris <samuel.kontris@pantheon.tech>
    Issue-ID: SDNC-803
    Change-Id: Id0582cd2a264e2538d5024b32d45e72058079e31
    Signed-off-by: Samuel Kontris <samuel.kontris@pantheon.tech>
    
    Former-commit-id: 92b51d3e828f9023d7bda78ef3a7b98bd25c82a6

---
+++ lighty/sdnc-lighty-application/src/main/java/org/onap/sdnc/oam/lighty/SdncLightyApplication.java
@@ -0,0 +1,198 @@
+/*
+ * ============LICENSE_START==========================================
+ * Copyright (c) 2019 PANTHEON.tech s.r.o.
+ * ===================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS
+ * OF ANY KIND, either express or implied. See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ============LICENSE_END============================================
+ *
+ */
+package org.onap.sdnc.oam.lighty;
+
+import io.lighty.core.controller.api.AbstractLightyModule;
+import io.lighty.core.controller.api.LightyController;
+import io.lighty.core.controller.api.LightyModule;
+import io.lighty.core.controller.impl.LightyControllerBuilder;
+import io.lighty.core.controller.impl.config.ConfigurationException;
+import io.lighty.core.controller.impl.config.ControllerConfiguration;
+import io.lighty.modules.northbound.restconf.community.impl.CommunityRestConf;
+import io.lighty.modules.northbound.restconf.community.impl.CommunityRestConfBuilder;
+import io.lighty.modules.northbound.restconf.community.impl.config.RestConfConfiguration;
+import io.lighty.modules.northbound.restconf.community.impl.util.RestConfConfigUtils;
+import io.lighty.modules.southbound.netconf.impl.NetconfTopologyPluginBuilder;
+import io.lighty.modules.southbound.netconf.impl.config.NetconfConfiguration;
+import io.lighty.modules.southbound.netconf.impl.util.NetconfConfigUtils;
+import org.onap.ccsdk.sli.core.lighty.common.CcsdkLightyUtils;
+import org.onap.ccsdk.sli.distribution.lighty.CcsdkLightyModule;
+import org.opendaylight.aaa.encrypt.impl.AAAEncryptionServiceImpl;
+import org.opendaylight.controller.md.sal.binding.api.DataBroker;
+import org.opendaylight.controller.md.sal.binding.api.MountPointService;
+import org.opendaylight.controller.md.sal.binding.api.NotificationPublishService;
+import org.opendaylight.controller.sal.binding.api.RpcProviderRegistry;
+import org.opendaylight.mdsal.binding.api.RpcProviderService;
+import org.opendaylight.mdsal.singleton.common.api.ClusterSingletonServiceProvider;
+import org.opendaylight.netconf.sal.connect.util.NetconfSalKeystoreService;
+import org.opendaylight.yang.gen.v1.config.aaa.authn.encrypt.service.config.rev160915.AaaEncryptServiceConfig;
+import org.opendaylight.yang.gen.v1.config.aaa.authn.encrypt.service.config.rev160915.AaaEncryptServiceConfigBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.netconf.keystore.rev171017.NetconfKeystoreService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * The implementation of the {@link io.lighty.core.controller.api.LightyModule} that groups all necessary components
+ * needed to start the SDNC lighty.io application.
+ */
+public class SdncLightyApplication extends AbstractLightyModule {
+
+    private static final Logger LOG = LoggerFactory.getLogger(SdncLightyApplication.class);
+
+    private ControllerConfiguration controllerConfiguration;
+    private RestConfConfiguration restConfConfiguration;
+    private NetconfConfiguration netconfSBPConfig;
+
+    private LightyController lightyController;
+    private CommunityRestConf communityRestConf;
+    private LightyModule netconfSouthboundPlugin;
+    private CcsdkLightyModule ccsdkLightyModule;
+    private SdncLightyModule sdncModule;
+
+    public SdncLightyApplication(ControllerConfiguration controllerConfiguration,
+            RestConfConfiguration restConfConfiguration, NetconfConfiguration netconfSBPConfig) {
+        this.controllerConfiguration = controllerConfiguration;
+        this.restConfConfiguration = restConfConfiguration;
+        this.netconfSBPConfig = netconfSBPConfig;
+    }
+
+    @Override
+    protected boolean initProcedure() {
+        // Start Lighty Controller with base OLD services
+        LightyControllerBuilder lightyControllerBuilder = new LightyControllerBuilder();
+        try {
+            lightyController = lightyControllerBuilder.from(controllerConfiguration).build();
+        } catch (ConfigurationException e) {
+            LOG.error("Exception thrown while starting Lighty controller!", e);
+            return false;
+        }
+        if (!CcsdkLightyUtils.startLightyModule(lightyController)) {
+            LOG.error("Unable to start Lighty controller!");
+            return false;
+        }
+
+        // Prepare needed ODL services
+        DataBroker dataBroker = lightyController.getServices().getControllerBindingDataBroker();
+        NotificationPublishService notificationPublishService = lightyController.getServices()
+                .getControllerBindingNotificationPublishService();
+        RpcProviderRegistry rpcProviderRegistry = lightyController.getServices().getControllerRpcProviderRegistry();
+        RpcProviderService rpcProviderService = lightyController.getServices().getRpcProviderService();
+        MountPointService mountPointService = lightyController.getServices().getControllerBindingMountPointService();
+        ClusterSingletonServiceProvider clusteringSingletonService =
+                lightyController.getServices().getClusterSingletonServiceProvider();
+
+        // Prepare AAA/security services
+        java.security.Security.addProvider(
+                new org.bouncycastle.jce.provider.BouncyCastleProvider()
+        );
+        AaaEncryptServiceConfig aaaEncryptConfig = createDefaultAaaEncryptServiceConfig();
+        AAAEncryptionServiceImpl aaaEncryptionService = new AAAEncryptionServiceImpl(aaaEncryptConfig, dataBroker);
+        NetconfSalKeystoreService netconfSalKeystoreService =
+                new NetconfSalKeystoreService(dataBroker, aaaEncryptionService);
+        lightyController.getServices().getRpcProviderService().registerRpcImplementation(NetconfKeystoreService.class,
+                netconfSalKeystoreService);
+
+        // Start RestConf
+        CommunityRestConfBuilder communityRestConfBuilder = new CommunityRestConfBuilder();
+        communityRestConf = communityRestConfBuilder
+                .from(RestConfConfigUtils.getRestConfConfiguration(restConfConfiguration,
+                        lightyController.getServices()))
+                .build();
+        if (!CcsdkLightyUtils.startLightyModule(communityRestConf)) {
+            LOG.error("Unable to start RestConf!");
+            return false;
+        }
+
+        // Start NetConf
+        try {
+            netconfSBPConfig = NetconfConfigUtils.injectServicesToTopologyConfig(
+                    netconfSBPConfig, lightyController.getServices());
+            netconfSBPConfig.setAaaService(aaaEncryptionService);
+        } catch (ConfigurationException e) {
+            LOG.error("Exception thrown while preparing configuration for NetConf!", e);
+            return false;
+        }
+        NetconfTopologyPluginBuilder netconfSBPBuilder = new NetconfTopologyPluginBuilder();
+        netconfSouthboundPlugin = netconfSBPBuilder
+                .from(netconfSBPConfig, lightyController.getServices())
+                .build();
+        if (!CcsdkLightyUtils.startLightyModule(netconfSouthboundPlugin)) {
+            LOG.error("Unable to start NetConf!");
+            return false;
+        }
+
+        // Start Lighty CCSDK
+        ccsdkLightyModule = new CcsdkLightyModule(dataBroker, notificationPublishService,
+                rpcProviderRegistry, aaaEncryptionService);
+        if (!CcsdkLightyUtils.startLightyModule(ccsdkLightyModule)) {
+            LOG.error("Unable to start CCSDK Lighty module!");
+            return false;
+        }
+
+        // Start Lighty SDNC
+        sdncModule = new SdncLightyModule(dataBroker, rpcProviderRegistry, notificationPublishService, mountPointService,
+                clusteringSingletonService, ccsdkLightyModule.getSliModule().getSvcLogicServiceImpl(),
+                rpcProviderService, false);
+        if (!CcsdkLightyUtils.startLightyModule(sdncModule)) {
+            LOG.error("Unable to start SDNC Lighty module!");
+            return false;
+        }
+
+        return true;
+    }
+
+    @Override
+    protected boolean stopProcedure() {
+        boolean stopSuccessful = true;
+
+        if (!CcsdkLightyUtils.stopLightyModule(sdncModule)) {
+            stopSuccessful = false;
+        }
+
+        if (!CcsdkLightyUtils.stopLightyModule(ccsdkLightyModule)) {
+            stopSuccessful = false;
+        }
+
+        if (!CcsdkLightyUtils.stopLightyModule(netconfSouthboundPlugin)) {
+            stopSuccessful = false;
+        }
+
+        if (!CcsdkLightyUtils.stopLightyModule(communityRestConf)) {
+            stopSuccessful = false;
+        }
+
+        if (!CcsdkLightyUtils.stopLightyModule(lightyController)) {
+            stopSuccessful = false;
+        }
+
+        return stopSuccessful;
+    }
+
+    private AaaEncryptServiceConfig createDefaultAaaEncryptServiceConfig() {
+        return new AaaEncryptServiceConfigBuilder()
+                .setEncryptKey("V1S1ED4OMeEh")
+                .setPasswordLength(12)
+                .setEncryptSalt("TdtWeHbch/7xP52/rp3Usw==")
+                .setEncryptMethod("PBKDF2WithHmacSHA1")
+                .setEncryptType("AES")
+                .setEncryptIterationCount(32768)
+                .setEncryptKeyLength(128)
+                .setCipherTransforms("AES/CBC/PKCS5Padding")
+                .build();
+    }
+
+}
---
+++ lighty/sdnc-lighty-docker/src/main/scripts/startODL.sh
@@ -0,0 +1,78 @@
+#!/bin/bash
+
+###
+# ============LICENSE_START=======================================================
+# openECOMP : SDN-C
+# ================================================================================
+# Copyright (C) 2017 AT&T Intellectual Property. All rights
+#                             reserved.
+# ================================================================================
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ============LICENSE_END=========================================================
+###
+
+
+# Install SDN-C platform components if not already installed and start container
+
+ODL_HOME=${ODL_HOME:-/opt/opendaylight/current}
+ODL_ADMIN_USERNAME=${ODL_ADMIN_USERNAME:-admin}
+ODL_ADMIN_PASSWORD=${ODL_ADMIN_PASSWORD:-Kp8bJ4SXszM0WXlhak3eHlcse2gAw84vaoGGmJvUy2U}
+SDNC_HOME=${SDNC_HOME:-/opt/onap/sdnc}
+SDNC_BIN=${SDNC_BIN:-/opt/onap/sdnc/bin}
+CCSDK_HOME=${CCSDK_HOME:-/opt/onap/ccsdk}
+SLEEP_TIME=${SLEEP_TIME:-120}
+MYSQL_PASSWD=${MYSQL_PASSWD:-openECOMP1.0}
+ENABLE_ODL_CLUSTER=${ENABLE_ODL_CLUSTER:-false}
+IS_PRIMARY_CLUSTER=${IS_PRIMARY_CLUSTER:-false}
+MY_ODL_CLUSTER=${MY_ODL_CLUSTER:-127.0.0.1}
+INSTALLED_DIR=${INSTALLED_FILE:-/opt/opendaylight/current/daexim}
+SDNRWT=${SDNRWT:-false}
+SDNRWT_BOOTFEATURES=${SDNRWT_BOOTFEATURES:-sdnr-wt-feature-aggregator}
+SDNR_NORTHBOUND=${SDNR_NORTHBOUND:-false}
+SDNR_NORTHBOUND_BOOTFEATURES=${SDNR_NORTHBOUND_BOOTFEATURES:-sdnr-northbound-all}
+export ODL_ADMIN_PASSWORD ODL_ADMIN_USERNAME
+
+echo "Settings:"
+echo "  ENABLE_ODL_CLUSTER=$ENABLE_ODL_CLUSTER"
+echo "  SDNC_REPLICAS=$SDNC_REPLICAS"
+echo "  SDNRWT=$SDNRWT"
+echo "  SDNR_NORTHBOUND=$SDNR_NORTHBOUND"
+
+#
+# Wait for database
+#
+echo "Waiting for mysql"
+until mysql -h dbhost -u root -p${MYSQL_PASSWD} mysql &> /dev/null
+do
+  printf "."
+  sleep 1
+done
+echo -e "\nmysql ready"
+
+echo "Installing SDN-C database"
+${SDNC_HOME}/bin/installSdncDb.sh
+
+echo "Installing SDN-C keyStore"
+${SDNC_HOME}/bin/addSdncKeyStore.sh
+
+if [ -x ${SDNC_HOME}/svclogic/bin/install.sh ]
+then
+    echo "Installing directed graphs"
+    ${SDNC_HOME}/svclogic/bin/install.sh
+fi
+
+cp /opt/opendaylight/current/certs/* /tmp
+
+nohup python ${SDNC_BIN}/installCerts.py &
+
+exec java -DSDNC_CONFIG_DIR=${SDNC_CONFIG_DIR} -jar opt/lighty/sdnc-lighty-application-1.6.0-SNAPSHOT/sdnc-lighty-application-1.6.0-SNAPSHOT.jar /opt/onap/sdnc/data/properties/lightySdncConfig.json
