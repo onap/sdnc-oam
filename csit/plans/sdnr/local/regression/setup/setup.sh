3115cc85802d62b6e8f054749c1b681547c1ceeb
demskeq8 <alexander.dehn@highstreet-technologies.com>
2021-06-02 15:50:12 +0200

    [SNDC-CSIT] Provide test deployment for SDNC/R
    Integration tests for wt feature set
    
    Issue-ID: SDNC-1562
    Signed-off-by: demskeq8 <alexander.dehn@highstreet-technologies.com>
    Change-Id: If579b6f23b3e6291dc49b10952b23da2626e45b7
    
    Former-commit-id: 3400748845b1e2947939e409fc6d1879b16de691

---
+++ csit/plans/sdnr/local/regression/setup/setup.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+#
+# Copyright 2016-2017 Huawei Technologies Co., Ltd.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# Modifications copyright (c) 2017 AT&T Intellectual Property
+# Modifications copyright (c) 2021 highstreet technologies GmbH Property
+#
+
+source ${WORKSPACE}/scripts/sdnr/sdnr-launch.sh
+onap_dependent_components_launch
+vescollector_launch
+nts_networkfunctions_launch
+sdnr_launch
+
+#Pass any variables required by Robot test suites in ROBOT_VARIABLES
+ROBOT_VARIABLES="--variablefile=${WORKSPACE}/plans/sdnr/local/environment/localhost.py"
+
+
